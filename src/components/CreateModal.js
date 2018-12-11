e08b3bb9ea03bace97f917ebdd43b502b40912df
Jordan Daniels <jordandaniels@Husayns-MBP.localdomain>
2018-12-10 18:16:57 -0800

    project info layout made

--- src/components/CreateModal.js
+++ src/components/CreateModal.js
@@ -6,12 +6,17 @@ import {
   MODE_CHANGE
 } from '../constants/actionTypes';
 
+const imagePlaceholder = require('../assets/plus.png');
+const removeImage = require('../assets/remove.png')
+
 class CreateModal extends React.Component {
   constructor() {
     super();
     this.state = {
       choosePlatform: true,
       projectInfo: false,
+      projectImage: "",
+      referenceFiles: [1,2],
       selected: {
         iOS: false,
         Android: false,
@@ -25,8 +30,10 @@ class CreateModal extends React.Component {
     this.choosePlatform = this.choosePlatform.bind(this);
     this.selectPlatform = this.selectPlatform.bind(this);
     this.requestClose = this.requestClose.bind(this);
+    this.onDrop = this.onDrop.bind(this);
    
   }
+  
 
   async componentWillMount() {
   }
@@ -55,6 +62,14 @@ class CreateModal extends React.Component {
          projectInfo: false
        })
     }
+  }
+
+  onDrop(files){
+    console.log(files, "files")
+    let ext = files[0].type.split("/").pop()
+    this.setState({
+      projectImage: files[0]
+    })
   }
 
   choosePlatform(){
@@ -108,12 +123,72 @@ class CreateModal extends React.Component {
     }
   }
 
+  renderReferenceFiles(){
+    let { referenceFiles } = this.state;
+    return referenceFiles.map(file => {
+      return (
+        <div style={{marginBottom: "10px"}}>
+         <input style={{display:"inline", width: "240px", margin: "0 10px 0 0"}} type="text" className="Rectangle"></input> 
+          <img src={removeImage}  style={{display: "inline", height: "30px", width: "30px", margin: 0}}></img>
+        </div>
+       
+      )
+    })
+  }
+
   projectInfo(){
+    let { projectImage } = this.state;
     if (this.state.projectInfo){
       return (
-        <div className="container modal-platform" style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-        <div className="row" style={{ marginBottom: "100px"}}>
-          <h2>projectInfo</h2>
+        <div className="container modal-platform ">
+          <div className="row" style={{ marginBottom: "50px"}}>
+            <h2>Project Information</h2>
+          </div>
+          <div className="row">
+            <div className="col-md-5 project-info" style={{marginRight: "50px"}}>
+              <div className="row">
+                <p>Name</p> 
+                <input type="text" className="Rectangle"></input>
+              </div>
+              <div className="row">
+                <p>Description</p>
+                <textarea className="Rectangle_Big"></textarea>
+              </div>
+              <div className="row">
+                <p>Client Name</p> 
+                <input type="text" className="Rectangle"></input>
+              </div>
+              <div className="row">
+                <p>Notes </p>
+                <textarea className="Rectangle_Big"></textarea>
+              </div>
+            </div>
+            <div className="col-md-5 project-info" >
+              <div className="row">
+                <p>LOGO </p>
+                <Dropzone className="Square" style={{border: "none", cursor: "pointer" }} onDrop={this.onDrop}>
+                  <img src={ projectImage ? projectImage.preview : imagePlaceholder } 
+                    style={{height: "32px", margin: "auto"}}
+                    onError={e => { 
+                    e.target.src = imagePlaceholder}}
+                  />
+                </Dropzone>
+              </div>
+              <div className="row ">
+                <p>PROJECT CREATOR </p>
+                <input type="text" className="Rectangle"></input>
+              </div>
+              <div className="row">
+                 <p>OPPURTUNITY</p> 
+                <input type="text" className="Rectangle"></input>
+              </div>
+              <div className="row">
+                  <p>REFERENCE FILES</p>
+                  <input style={{display:"inline", width: "240px", margin: "0 10px 0 0", marginBottom: "10px"}} type="text" className="Rectangle"></input> 
+                  <img src={imagePlaceholder}  style={{display: "inline", height: "30px", width: "30px", margin: 0}}></img>
+                  {this.renderReferenceFiles()}
+              </div>
+          </div>
         </div>
       </div>
       )
@@ -126,6 +201,7 @@ class CreateModal extends React.Component {
     this.setState({
       choosePlatform: true,
       projectInfo: false,
+      projectImage: "",
       selected: {
         iOS: false,
         Android: false,
