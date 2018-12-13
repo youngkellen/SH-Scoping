c8e53ae2faccde5da03a56945085fcfe3be48788
Jordan Daniels <jordandaniels@Husayns-MBP.localdomain>
2018-12-12 18:07:58 -0800

    fixed issues with split screen scrolling

--- src/components/Builder/FeatureSets.js
+++ src/components/Builder/FeatureSets.js
@@ -5,20 +5,23 @@ import { connect } from 'react-redux';
 const mapStatetoProps = state => ({viewMode: state.viewMode})
 
 class FeatureSets extends Component {
-
+  
   getElementHeight(){
     let height = document.getElementById("General").getBoundingClientRect().height
-    console.log(height, "height")
+    console.log(height, "height bitch")
   }
 
     render() {
+      let { viewMode } = this.props;
+      let height = viewMode.split ? "40vh" : "76vh"
       return (
         <div className="row">
-          <div className="col-md-12"  style={{height: "100vh"}}>
-          <div className ="row column_head" style={{textAlign: "center"}}>
+         <div className ="row column_head" style={{textAlign: "center"}}>
             Feature Sets
           </div>
-          <div className="row layout-pane-scroll">
+          <div className="col-md-12"  style={{height: height, overflow: "scroll"}}>
+         
+          <div className="row layout-pane-scroll"  style={{overflowY: "scroll"}}>
               <button className="collapsible" onClick={() => this.getElementHeight()}>General</button>
               
               <div className="content" id="General">
