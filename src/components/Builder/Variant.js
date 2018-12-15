c73dc1ef0d82aeaa73f81a6c67a5940700240d61
Jordan Daniels <jordandaniels@Husayns-MBP.localdomain>
2018-12-14 16:11:05 -0800

    estimate input can be editted can change colors accordingly

--- src/components/Builder/Variant.js
+++ src/components/Builder/Variant.js
@@ -1,16 +1,26 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
 const mapStatetoProps = state => ({ viewMode: state.viewMode });
 
-class Variant extends PureComponent {
+class Variant extends Component {
     constructor() {
         super()
         this.state = {
             addToScope: false,
             duplicate: false,
-            menuClick: false
+            menuClick: false,
+            estimate: false,
+            buttonData:[
+                {hours: 2, platform: "iOS", select: true}, 
+                {hours: 0, platform: "Android"},
+                {hours: 2, platform: "Hybrid"},
+                {hours: 1, platform: "Web"},
+                {hours: 2, platform: "Backend"},
+                {hours: 2, platform: "QA"},
+                {hours: 0, platform: "Design", select: true}
+            ]
         }
     }
 
@@ -54,9 +64,62 @@ class Variant extends PureComponent {
 
     }
 
+    selectEstimate(data){
+        console.log(data, "data");
+        let { buttonData } = this.state;
+        let newData = Object.assign({}, data, {select: !data.select})
+        let index = buttonData.map(e => e.platform).indexOf(data.platform);
+        buttonData.splice(index, 1, newData)
+        console.log(buttonData, "button Data")
+        this.setState({
+            buttonData
+        })
+    }
+
+    changeEstimateHours(data, val){
+        let { buttonData } = this.state;
+        let newData = Object.assign({}, data, {hours: Number(val)})
+        let index = buttonData.map(e => e.platform).indexOf(data.platform);
+        buttonData.splice(index, 1, newData)
+        console.log(buttonData, "button Data")
+        this.setState({
+            buttonData
+        })
+
+    }
+
+
+    renderEstimateButtons() {
+        let { buttonData } = this.state;
+        return buttonData.map(data => {
+            return (
+                <div className="col-md-1 content_row" >
+                        <div>
+                            <img 
+                                src={require(`../../assets/${data.select ? `check-black` : `empty`}.png`)}
+                                onClick={() => this.selectEstimate(data)}
+                             />
+                        </div>
+                        <div style={data.select ? {color: "black"} : {color: "lightgray"}}>
+                            <input 
+                                style={data.select && !data.hours ? {backgroundColor: "#ffc2c2"}: {}} 
+                                defaultValue={Number(data.hours)} 
+                                type="number"
+                                onChange={e => this.changeEstimateHours(data, e.target.value)}
+                                readOnly={data.select ? false : true}
+                            />
+                        </div>
+                        <div style={data.select ? {color: "black"} : {color: "lightgray"}}>
+                            <p>{data.platform}</p>
+                        </div>
+                </div>
+            )
+        })
+    }
+
 
     render() {
-        let { menuClick } = this.state;
+        let { menuClick, estimate } = this.state;
         let dropDownColor = "#656565"
         console.log(menuClick, "menu click")
         return (
@@ -78,13 +141,13 @@ class Variant extends PureComponent {
                             <div className="col-md-9">
                                 {this.renderDuplicate()}
                             </div>
-                            <div className="col-md-3" >
-                                <img 
+                            <div className="col-md-3" style={{ cursor: "pointer" }} onClick={() => this.setState(prevState => ({ menuClick: !prevState.menuClick }))} >
+                                <img
                                     style={{ width: "2px" }}
-                                    src={require("../../assets/menu.png")} 
-                                    onClick={() => this.setState(prevState => ({ menuClick: !prevState.menuClick }))}
+                                    src={require("../../assets/menu.png")}
+
                                 />
-                                <div class="dropdown-menu dropdown-menu-right" style={menuClick ? {display: "block"} : {display: "none"}}>
+                                <div class="dropdown-menu dropdown-menu-right" style={menuClick ? { display: "block" } : { display: "none" }}>
                                     <a class="dropdown-item">Add To Library</a>
                                     <a class="dropdown-item">Update in Library</a>
                                 </div>
@@ -107,7 +170,23 @@ class Variant extends PureComponent {
                         <img src={require("../../assets/plus-black.png")} />
                         <p>notes</p>
                     </div>
-                    <div className="col-md-2">estimates</div>
+                    <div className="col-md-2" style={{ cursor: "pointer" }} onClick={() => this.setState(prevState => ({ estimate: !prevState.estimate }))} >
+                        <p>Estimates</p>
+                        <div className={estimate ? "arrow-up" : "arrow-down"}></div>
+                    </div>
+                </div>
+                <div className="container row variant_row">
+                    <div className="col-md-12 estimate_content" style={estimate ? { display: "block" } : { display: "none" }}>
+                        <div className="row">
+                            <p>Estimates (HOURS):</p>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6">
+                            {this.renderEstimateButtons()}
+                            </div>
+                        </div>
+                       
+                    </div>
                 </div>
             </div>
         )
